1778eb82bd66dc09d44c632df191737e83fd77e1
aboab <111141901+KonstantinRevin@users.noreply.github.com>
2023-08-16 18:00:20 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,131 @@
+
+function playSound1() {
+  var audio = new Audio('audio/1 a_mne_pohui.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound2() {
+  var audio = new Audio('audio/2 ahahah_durak.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound3() {
+  var audio = new Audio('audio/3 a_nu_davai.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound4() {
+  var audio = new Audio('audio/4 a_nu_a_interesno.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound5() {
+  var audio = new Audio('audio/5 a_nu_eto_ban_blyat.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound6() {
+  var audio = new Audio('audio/6 aaa_nu_yasno.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound7() {
+  var audio = new Audio('audio/7 allo_svetlana.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound8() {
+  var audio = new Audio('audio/8 cerkovnaya_gadza.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound9() {
+  var audio = new Audio('audio/9 delo_sdelano.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound10() {
+  var audio = new Audio('audio/10 dryan.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound11() {
+  var audio = new Audio('audio/11 dryan_blyat.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound12() {
+  var audio = new Audio('audio/12 durachok_daun_loh.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound13() {
+  var audio = new Audio('audio/13 ebany_kompot.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound14() {
+  var audio = new Audio('audio/14 ebany_krot.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound15() {
+  var audio = new Audio('audio/15 ebat_debil.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound16() {
+  var audio = new Audio('audio/16 ebat_kak_krasivo.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound17() {
+  var audio = new Audio('audio/17 esli_ya_nachnu_proyavlyat_agressiyu.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound18() {
+  var audio = new Audio('audio/18 eto_ty_komu_skazal.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound19() {
+  var audio = new Audio('audio/19 gadza.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound20() {
+  var audio = new Audio('audio/20 gadza_demon.mp3.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound21() {
+  var audio = new Audio('audio/21 gadza_kosmicheskaya.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound22() {
+  var audio = new Audio('audio/22 ha_ha_dolboeb_idi_nahui.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound23() {
+  var audio = new Audio('audio/23 hahaha_sho.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound24() {
+  var audio = new Audio('audio/24 hochu_masla.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound25() {
+  var audio = new Audio('audio/25 huyandok-1.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound26() {
+  var audio = new Audio('audio/26 i_am_dodik_i_am_daun.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound27() {
+  var audio = new Audio('audio/27 izvinites_i_ya_poshel.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound28() {
+  var audio = new Audio('audio/28 kto_budet_pervim_izinyatsya.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound29() {
+  var audio = new Audio('audio/29 lovi_moyu_gadzu.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound30() {
+  var audio = new Audio('audio/30 molitva.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound31() {
+  var audio = new Audio('audio/31 sluzhit_padla.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+function playSound32() {
+  var audio = new Audio('audio/11 dryan_blyat.mp3'); // Замените 'sound.mp3' на путь к звуковому файлу
+  audio.play();
+}
+
+
